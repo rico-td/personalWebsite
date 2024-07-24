07c9edbe692d2d7ca02acf62221e7709c0b61467
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-07-24 22:12:21 +0200

    try deployment

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -2,25 +2,42 @@ import React from "react";
 
 const ContactBanner = () => {
    return (
-      <button className="contact-copy-button">
-         <div className="text-white contact-banner">
-            <div className="contact-mover" style={{ animationDuration: "22s" }}>
-               <span>schroeder.riccardo@gmail.com</span>
+      <>
+         <button className="contact-copy-button">
+            <div className="text-white contact-banner">
+               <div
+                  className="contact-mover"
+                  style={{ animationDuration: "22s" }}
+               >
+                  <span>schroeder.riccardo@gmail.com</span>
+               </div>
+               <div
+                  className="contact-mover"
+                  style={{ animationDuration: "22s" }}
+               >
+                  <span>schroeder.riccardo@gmail.com</span>
+               </div>
+               <div
+                  className="contact-mover"
+                  style={{ animationDuration: "22s" }}
+               >
+                  <span>schroeder.riccardo@gmail.com</span>
+               </div>
+               <div
+                  className="contact-mover"
+                  style={{ animationDuration: "22s" }}
+               >
+                  <span>schroeder.riccardo@gmail.com</span>
+               </div>
+               <div
+                  className="contact-mover"
+                  style={{ animationDuration: "22s" }}
+               >
+                  <span>schroeder.riccardo@gmail.com</span>
+               </div>
             </div>
-            <div className="contact-mover" style={{ animationDuration: "22s" }}>
-               <span>schroeder.riccardo@gmail.com</span>
-            </div>
-            <div className="contact-mover" style={{ animationDuration: "22s" }}>
-               <span>schroeder.riccardo@gmail.com</span>
-            </div>
-            <div className="contact-mover" style={{ animationDuration: "22s" }}>
-               <span>schroeder.riccardo@gmail.com</span>
-            </div>
-            <div className="contact-mover" style={{ animationDuration: "22s" }}>
-               <span>schroeder.riccardo@gmail.com</span>
-            </div>
-         </div>
-      </button>
+         </button>
+      </>
    );
 };
 
