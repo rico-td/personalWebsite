943f741ed3b8b3b1d1b6eebfc4c27e7de1dfffce
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-07-24 22:04:35 +0200

    create the contact banner component and try some styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ContactBanner from "./components/ContactBanner";
+
+function App() {
+   return (
+      <>
+         <ContactBanner />
+      </>
+   );
+}
+
+export default App;
---
+++ src/components/ContactBanner.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ContactBanner = () => {
+   return (
+      <button className="contact-copy-button">
+         <div className="text-white contact-banner">
+            <div className="contact-mover" style={{ animationDuration: "22s" }}>
+               <span>schroeder.riccardo@gmail.com</span>
+            </div>
+            <div className="contact-mover" style={{ animationDuration: "22s" }}>
+               <span>schroeder.riccardo@gmail.com</span>
+            </div>
+            <div className="contact-mover" style={{ animationDuration: "22s" }}>
+               <span>schroeder.riccardo@gmail.com</span>
+            </div>
+            <div className="contact-mover" style={{ animationDuration: "22s" }}>
+               <span>schroeder.riccardo@gmail.com</span>
+            </div>
+            <div className="contact-mover" style={{ animationDuration: "22s" }}>
+               <span>schroeder.riccardo@gmail.com</span>
+            </div>
+         </div>
+      </button>
+   );
+};
+
+export default ContactBanner;
