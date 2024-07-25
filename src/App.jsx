1668074bedd1d868635cea893343eb86650f9655
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-07-25 22:58:45 +0200

    comment out the test dives

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
    return (
       <>
          <ContactBanner />
-         <div className="h-[100vh] bg-black"></div>
+         {/* <div className="h-[100vh] bg-black"></div>
          <div className="h-[100vh] bg-white"></div>
          <div className="h-[100vh] bg-red-500"></div>
-         <div className="h-[100vh] bg-blue-500"></div>
+         <div className="h-[100vh] bg-blue-500"></div> */}
       </>
    );
 }
