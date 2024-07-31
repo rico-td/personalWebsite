d69e7e2340dcf3d1860138917f7bdb5a6ec48b57
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-07-31 14:45:48 +0200

    delete some stuff, updated deploy.yml file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/personalWebsite/",
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-    },
-  },
 });
