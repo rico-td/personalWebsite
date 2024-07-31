7ea44bbfaa55bfbe58a9b249ee9912909ca4ebf1
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-07-31 12:55:52 +0200

    try deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personalWebsite/',
-})
+  base: "/personalWebsite/",
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+});
