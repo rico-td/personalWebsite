1aa7848d6a35c376ee99eb7322ad3f1435b82dfa
rico-techstarter <ricotech.dev+github@gmail.com>
2024-08-06 16:44:49 +0200

    implement banner and make it responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Hero.css";
+import { IoArrowRedoOutline } from "react-icons/io5";
 
 import bg from "../../../public/assets/images/bg.png";
 
@@ -8,9 +9,14 @@ import ContactBanner from "../ContactBanner/ContactBanner";
 const Hero = () => {
   return (
     <main className="hero-wrapper">
-      <div className="img-wrapper">
-        <img className="hero-img" src={bg} alt="" />
+      <div className="container-heading">
+      <IoArrowRedoOutline size={35}  style = {{transform: 'rotate(50deg)' }}/>
+      <h1 className="hero-heading"><span>Frontend Developer</span></h1>
       </div>
+     
+      {/* <div className="img-wrapper">
+        <img className="hero-img" src={bg} alt="" />
+      </div> */}
     </main>
   );
 };
